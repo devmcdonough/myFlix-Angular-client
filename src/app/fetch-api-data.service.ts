90b4b9171cc07d14fd28b81f75ff11fb73abe32b
devmcdonough <devmcdonough@gmail.com>
2024-07-05 16:24:20 -0400

    Fixed fetch-api-data issue

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+// Declaring the api url that will provide data for the client app
+const apiUrl = 'need_url_here';
+
+// Tells Angular that the service will be available everywhere
 @Injectable({
   providedIn: 'root'
 })
 export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+  // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Making the API call for the user registration endpoint
+  // Observable is a type of promise that says we will return this type, in this case being any
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Passes HTTP header when making API call
+  public getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // Non-typed response extraction
+  private extractResponseData(res: any): any {
+    const body = res;
+    return body || {};
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again soon.');
+  }
 }
